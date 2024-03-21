c404075e3a526067c66ba0c8e9412dac1827e214
Ramdani <xhijack@gmail.com>
2024-03-21 09:08:19 +0700

    add priod

---
+++ school/school/doctype/priod/test_priod.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, PT Sopwer Teknologi Indonesia and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPriod(FrappeTestCase):
+	pass
