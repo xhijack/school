db2cdb964511e57c1d62f65824aa53f06248f195
Ramdani <xhijack@gmail.com>
2024-04-22 08:48:21 +0700

    add schedule

---
+++ school/school/doctype/fees_schedule/fees_schedule.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2024, PT Sopwer Teknologi Indonesia and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Fees Schedule", {
+    refresh(frm){
+            frm.add_custom_button(__('Get All Students'), function() {
+                student_class = [];
+                frm.doc.student_class.forEach(element => {
+                    student_class.push(element.class_student); 
+                });
+                console.log(student_class)
+                frappe.call({
+                    method: 'school.school.doctype.fees_schedule.fees_schedule.get_all_students',
+                    args: {
+                        academic_year: frm.doc.academic_year,
+                        student_class: student_class
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            frm.set_value('students', r.message);
+                            frm.refresh_field('students');
+                            console.log(r.message)
+                        }
+                    }
+                });
+            });
+            frm.add_custom_button(__('Create Fees'), function() {
+                frappe.call({
+                    method: 'school.school.doctype.fees_schedule.fees_schedule.create_fees',
+                    args: {
+                        fees_schedule: frm.doc.name
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            frappe.msgprint('Membuat pembayaran berhasil');
+                        }
+                    }
+                });
+            });
+        
+    }
+})
+
+frappe.ui.form.on("Fees Schedule Class", {
+	class: function(frm, cdt, cdn) {
+           
+	}
+});
+
+function calculate_total_students(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    frappe.model.set_value(row.doctype, row.name, 'total_students', row.rate * row.qty);
+    calculate_total(frm);
+}
+
---
+++ school/school/doctype/fees_schedule/fees_schedule.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2024, PT Sopwer Teknologi Indonesia and contributors
+# For license information, please see license.txt
+
+import json
+import frappe
+from frappe.model.document import Document
+
+
+class FeesSchedule(Document):
+	def validate(self):
+		self.total = sum([s.total_fees for s in self.students])
+
+
+
+@frappe.whitelist()
+def get_all_students(academic_year, student_class):
+	student_class = json.loads(student_class)
+	# import pdb
+	# pdb.set_trace()
+ 
+	students_with_fees = []
+	for sc in student_class:
+		student_class_ = frappe.get_all('Student Class', {'academic_year': academic_year, 'student_class': sc})
+		if student_class_:
+			sc_ = frappe.get_doc('Student Class', student_class_[0].name)
+			students = sc_.students
+			for student in students:
+				fs = frappe.get_all('Fee Structure', {'student': student.student, 'academic_year': academic_year})
+				if fs:
+					fee_structure = frappe.get_doc('Fee Structure', fs[0].name)
+					students_with_fees.append({
+						'student': student.student,
+						'full_name': student.full_name,
+						'student_class': sc,
+						'fee_structure': fee_structure.name,
+						'total_fees': fee_structure.total
+					})
+	
+	return students_with_fees				
+
+@frappe.whitelist()
+def create_fees(fees_schedule):
+	fs = frappe.get_doc('Fees Schedule', fees_schedule)
+	for student in fs.students:
+		fee = frappe.new_doc('Fees')
+		fee.student = student.student
+		fee.date = fs.posting_date
+		fee.academic_year = fs.academic_year
+		fee.priod = fs.priod
+		fee.payment_due_date = fs.due_date
+		fee.student_class = student.student_class
+		fee.student_name = student.full_name
+		fee.fee_structure = student.fee_structure
+		fee.total_fees = student.total_fees
+
+		for fee_structure in frappe.get_all('Fee Structure', {'name': student.fee_structure}):
+			fee_structure_ = frappe.get_doc('Fee Structure', fee_structure.name)
+			for fee_structure_detail in fee_structure_.fee_structure_detail:
+				fee_detail = fee.append('fees_detail', {})
+				fee_detail.fees_category = fee_structure_detail.fees_category
+				fee_detail.amount = fee_structure_detail.amount
+
+		fee.submit()
+	return "success"
+		
